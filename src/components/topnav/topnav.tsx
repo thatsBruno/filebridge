9feb61e58afe0cbb82ef1f64ca1195977bf66caa
Bruno Ribeiro <brunoandreribeiro18@gmail.com>
2024-09-12 22:50:53 +0100

    add user to topnav

--- src/components/topnav/topnav.tsx
+++ src/components/topnav/topnav.tsx
@@ -2,18 +2,32 @@ import { FaPlus, FaSignOutAlt } from 'react-icons/fa';
 import styles from './topnav.module.css';
 import { FaBucket } from 'react-icons/fa6';
 import supabase from '../../lib/supabase';
+import { useEffect, useState } from 'react';
+import { User } from '@supabase/supabase-js';
 
 export default function TopNav() {
-    const userName = "John Doe"; // Replace with actual user name or state
+    const [user, setUser] = useState<User | null>(null)
+
+    const fetchUserData = async () => {
+        const { data: { user } } = await supabase.auth.getUser();
+        setUser(user);
+        if(user != null){
+            return user.user_metadata;
+        }
+    }
 
     async function handleLogout() {
         const { error } = await supabase.auth.signOut();
         console.log("Logout clicked", {error}); // Moved inside the function
     } 
 
+    useEffect(() => {
+      fetchUserData();
+    }, ) // Added dependency array to run only on mount
+    
     return (
         <nav className={styles.topNav}>
-            <span className={styles.userName}>{userName}</span>
+            <span className={styles.userName}>{user?.email}</span>
             <h1 className={styles.appName}>Filebridge</h1>
             <div className={styles.buttonGroup}>
                 <button className={`${styles.button} ${styles.createButton}`} title="Create Bucket">
